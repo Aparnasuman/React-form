8c055651910c170f8d53064f3599842b09d5b94d
Aparna Suman <Aparnasuman2@gmail.com>
2020-10-11 22:21:38 +0530

    action button work

--- signup/src/App.js
+++ signup/src/App.js
@@ -6,9 +6,16 @@ class App extends React.Component{
     this.state  = {
       Firstname:'',
       Lastname:'',
-      items:[]
+      items:[],
+      fields:[""]
     }
 
+  }
+
+  onClickButtonAdder=()=> {
+    this.setState({
+      fields: ["", ...this.state.fields]
+    });
   }
   onhandlechange(e){
     this.setState({
@@ -34,19 +41,21 @@ console.log(items)
   return(
     <div className="container ">
       <div className="row justify-content-md-center">
-        <div className="col-md-4 col=md-6">
-           <form>
-             <div className="form-group">
-             <label >FirstName</label>
-             <input type="text" className="form-control" id="form-input1" onChange={(e)=>this.onhandlechange(e)} placeholder="FirstName "></input>
+        <div className="col-md-4  mt-3">
+        {this.state.fields.map((value, index) => (<form    value={value}
+            key={index}>
+             <div className="form-group  col=md-6 d-flex">
+             <label className="mr-3">x</label>
+             <input type="text" className="form-control" id="form-input1" onChange={(e)=>this.onhandlechange(e)} placeholder="x "></input>
           </div>
-            <div className="form-group">
-            <label >LastName</label>
-            <input type="text" className="form-control" id="form-input2" onChange={(e)=>this.oninputchange(e)}  placeholder="LastName"></input>
+            <div className="form-group col=md-6 d-flex">
+            <label className="mr-3" >y</label>
+            <input type="text" className="form-control" id="form-input2" onChange={(e)=>this.oninputchange(e)}  placeholder="y"></input>
              </div>
-             <button type="button" className="btn btn-primary mr-3" onClick={this.addItem}>Submit</button>
-             <button type="button" className="btn btn-primary">Action</button>
            </form>
+            ))}
+           <button type="button" className="btn btn-primary mr-3 ml-4" onClick={this.addItem}>Submit</button>
+             <button type="button" className="btn btn-primary" onClick={this.onClickButtonAdder}>Action</button>
         </div>
       </div>
        
