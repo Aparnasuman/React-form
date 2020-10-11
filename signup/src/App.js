a0a73d0c0182024a31086b5e66dcd3642b562a33
Aparna Suman <Aparnasuman2@gmail.com>
2020-10-11 20:55:32 +0530

    form of reaact

--- signup/src/App.js
+++ signup/src/App.js
@@ -1,30 +1,61 @@
 import React from 'react';
-import { Button } from 'reactstrap';
-import './App.css';
-import {Alert} from 'reactstrap';
+class App extends React.Component{
+ 
+  constructor(props){
+    super(props);
+    this.state  = {
+      Firstname:'',
+      Lastname:'',
+      items:[]
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container">
-      <Button color="primary">HELLO</Button>
-      <Alert color="success">
-        This is a success alert — check it out!
-        This is a primary alert with <a href="#" className="alert-link">hi</a> HELLO
-        <h4 className="alert-heading">Well done!</h4>
-        <p>
-          Aww yeah, you successfully read this important alert message. This example text is going
-          to run a bit longer so that you can see how spacing within an alert works with this kind
-          of content.
-        </p>
-        <hr />
-        <p className="mb-0">
-          Whenever you need to, be sure to use margin utilities to keep things nice and tidy.
-        </p>
-      </Alert>
-    </div>
-    </div>
-  );
+  }
+  onhandlechange(e){
+    this.setState({
+      Firstname:e.target.value
+    
+    });
+  }
+  oninputchange(e){
+    this.setState({
+      Lastname:e.target.value
+    
+    });
+  }
+addItem=()=>{
+  let items =this.state.items;
+  items.push([this.state.Firstname, this.state.Lastname]);
+  this.setState({
+  items
+  })
+console.log(items)
 }
+  render(){
+  return(
+    <div className="container ">
+      <div className="row justify-content-md-center">
+        <div className="col-md-4 col=md-6">
+           <form>
+             <div className="form-group">
+             <label >FirstName</label>
+             <input type="text" className="form-control" id="form-input1" onChange={(e)=>this.onhandlechange(e)} placeholder="FirstName "></input>
+          </div>
+            <div className="form-group">
+            <label >LastName</label>
+            <input type="text" className="form-control" id="form-input2" onChange={(e)=>this.oninputchange(e)}  placeholder="LastName"></input>
+             </div>
+             <button type="button" className="btn btn-primary mr-3" onClick={this.addItem}>Submit</button>
+             <button type="button" className="btn btn-primary">Action</button>
+           </form>
+        </div>
+      </div>
+       
+    </div>
+  )
+  }
+
+
+}
+
 
 export default App;
